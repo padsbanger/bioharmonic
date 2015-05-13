6a6c074c42db4e1dd7655e1fb10dd504baf8474a
Michał Lach <kontakt@michal-lach.pl>
2015-05-13 14:01:30 +0200

    handle audio properly

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
-  window.addEventListener('DOMContentLoaded', function() {
-        new QueryLoader2(document.querySelector("body"), {
-            barColor: "#efefef",
-            backgroundColor: "#111",
-            percentage: true,
-            barHeight: 1,
-            minimumTime: 200,
-            fadeOutTime: 1000
-        });
-    });
+// AUDIO FILES
+
+var sounds = ['forest', 'pad', 'rain', 'thunder', 'wind'];
+
+// GENERATE AUDIO OBJECTS
+
+var generateSounds = function(soundsArray) {
+  var soundsObject = {};
+
+  for (var i = 0; i < soundsArray.length; i++) {
+    soundsObject[soundsArray[i]] = new Audio('sound/' + soundsArray[i].toUpperCase() + '.mp3');
+    soundsObject[soundsArray[i]].loop = true;
+    soundsObject[soundsArray[i]].preload = "auto";
+  }
+  return soundsObject;
+};
+
+// HANDLE SINGLE BUTTON
+
+var handleElement = function(sound, soundsObject) {
+  var button = document.getElementById(sound);
+  button.addEventListener('click', function() {
+    button.classList.toggle('active');
+    if (soundsObject[sound].paused) {
+      soundsObject[sound].play()
+    } else {
+      soundsObject[sound].pause();
+    }
+  });
+}
+
+// ATTACH HANDLERS TO BUTTONS
+
+var attachHandlers = function() {
+  var soundsObject = generateSounds(sounds);
+
+  for (var sound in soundsObject) {
+    if (soundsObject.hasOwnProperty(sound)) {
+      handleElement(sound, soundsObject);
+    }
+  };
+
+}();
